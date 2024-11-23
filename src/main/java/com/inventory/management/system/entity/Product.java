096d23ec5db04eae5244c96db1f20972ffb5a190
jagadeesh82001 <jagadeeshgopi001@gmail.com>
2024-11-23 16:30:31 +0530

    entity created

---
+++ src/main/java/com/inventory/management/system/entity/Product.java
@@ -0,0 +1,30 @@
+package com.inventory.management.system.entity;
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private double price;
+
+
+}
